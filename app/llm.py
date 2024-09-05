8280f38e97e67f5bb81a91a01b47da64516495c3
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-05 17:51:23 +0700

    2

--- app/llm.py
+++ app/llm.py
@@ -1,24 +1,54 @@
 import logging
+from operator import itemgetter
 from typing import List
 
 from langchain_core.chat_history import (
     BaseChatMessageHistory,
     InMemoryChatMessageHistory,
 )
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage, trim_messages
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 
 class LLM:
     __embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
-    __chat_completion_model = ChatOpenAI(model="gpt-3.5-turbo")
     __store = {}
     __config = {"configurable": {"session_id": "main"}}
 
     def __init__(self):
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "You are a helpful assistant. Answer all questions to the best of your ability.",
+                ),
+                MessagesPlaceholder(variable_name="messages"),
+            ]
+        )
+
+        chat_completion_model = ChatOpenAI(model="gpt-3.5-turbo")
+        trimmer = trim_messages(
+            max_tokens=65,
+            strategy="last",
+            token_counter=chat_completion_model,
+            include_system=True,
+            allow_partial=False,
+            start_on="human",
+        )
+
+        chain = (
+            RunnablePassthrough.assign(messages=itemgetter("messages") | trimmer)
+            | prompt
+            | chat_completion_model
+        )
+
         self.__with_message_history = RunnableWithMessageHistory(
-            self.__chat_completion_model, self.__get_session_history
+            chain,
+            self.__get_session_history,
+            input_messages_key="messages",
         )
 
     def __get_session_history(self, session_id: str) -> BaseChatMessageHistory:
@@ -36,7 +66,7 @@ class LLM:
     def complete_chat(self, content: str) -> str:
         try:
             response = self.__with_message_history.invoke(
-                [HumanMessage(content=content)],
+                {"messages": [HumanMessage(content=content)]},
                 config=self.__config,
             )
             return response.content
