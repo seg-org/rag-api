bb3ee10b23d7f5b9f4de1240c310c8f874beca19
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-05 14:51:57 +0700

    conf

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+from .config import config
+from .main import app
+
+__all__ = ["app", "config"]
---
+++ app/config.py
@@ -0,0 +1,30 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class AppConfig:
+    env = os.getenv("APP_ENV")
+    port = os.getenv("APP_PORT")
+
+
+class LangchainConfig:
+    api_key = os.getenv("LANGCHAIN_API_KEY")
+    is_tracing = os.getenv("LANGCHAIN_TRACING_V2")
+
+
+class DbConfig:
+    url = os.getenv("DB_URL")
+
+
+class Config:
+    app = AppConfig()
+    langchain = LangchainConfig()
+    db = DbConfig()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    embedding_api_host = os.getenv("EMBEDDING_API_HOST")
+
+
+config = Config()
