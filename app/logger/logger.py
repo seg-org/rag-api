0b3fae374333da046a88b1c781241ffeb6101124
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-11 15:37:46 +0700

    color log

--- app/logger/logger.py
+++ app/logger/logger.py
@@ -1,10 +1,26 @@
 import logging
 
+import colorlog
+
+formatter = colorlog.ColoredFormatter(
+    "%(log_color)s%(levelname)s:%(name)s:%(message)s",
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "bold_red",
+    },
+)
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.StreamHandler(),
+        handler,
     ],
 )
 
