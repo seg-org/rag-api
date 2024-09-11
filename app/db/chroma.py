b5e443d9718c94c1ccd60ba2f7157f6ea8366c73
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-11 22:24:31 +0700

    chroma try catch

--- app/db/chroma.py
+++ app/db/chroma.py
@@ -1,18 +1,14 @@
 from logging import Logger
-from typing import List
 
 from chromadb.config import Settings
 from config import config
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
 from langchain_nomic.embeddings import NomicEmbeddings
-from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 class DB:
-    __embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
-
     def __init__(self, log: Logger):
         self.log = log
         settings = Settings(
@@ -30,21 +26,25 @@ class DB:
         self.retriever = self.vectorstore.as_retriever()
 
     def add_web(self, url: str):
-        docs_list = WebBaseLoader(url).load()
+        try:
+            docs_list = WebBaseLoader(url).load()
+            text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=250, chunk_overlap=0
+            )
+            doc_splits = text_splitter.split_documents(docs_list)
 
-        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-            chunk_size=250, chunk_overlap=0
-        )
-        doc_splits = text_splitter.split_documents(docs_list)
-
-        self.vectorstore.add_documents(documents=doc_splits)
+            self.vectorstore.add_documents(documents=doc_splits)
+            self.log.info("Web document added to database:", url)
+            return "Web document added successfully: " + url
+        except Exception as e:
+            self.log.error(f"Error adding web document: {e}")
+            return ""
 
     def add_text(self, text: str):
         try:
             self.vectorstore.add_texts(texts=[text])
-            self.log.info(
-                "Text added to database",
-            )
+            self.log.info("Text added to database:", text)
+            return "Text added successfully: " + text
         except Exception as e:
             self.log.error(f"Error adding text: {e}")
             return ""
@@ -55,20 +55,11 @@ class DB:
             return results
         except Exception as e:
             self.log.error(f"Error getting all texts: {e}")
-            return None
+            return ""
 
     def get_relevant_text(self, text: str):
         try:
-            # embeddings = self.embed(text)
-            # results = self.retriever.get_relevant_documents(embeddings=embeddings)
             results = self.retriever.invoke(text)
             return [r.page_content for r in results]
         except Exception as e:
             self.log.error(f"Error getting relevant text: {e}")
-
-    def __embed(self, text: str) -> List[float]:
-        try:
-            return self.__embedding_model.embed_query(text)
-        except Exception as e:
-            self.log.error(f"Error embedding text: {e}")
-            return []
